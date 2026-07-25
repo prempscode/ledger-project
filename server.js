8188a65716708996c241166685ac8d2edf00cf38
premdev <prempravash65@gmail.com>
2026-07-26 02:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const connectDB = require("./src/config/db");
 
 const app = require("./src/app");
+const connectToDB = require("./src/config/db");
 
-connectDB();
-
-app.listen(3000, () => {
-  console.log("server running port 3000");
+connectToDB();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
