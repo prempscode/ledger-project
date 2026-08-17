3537c41ab76b1e168f2a96aebffb56ace1e9b798
premdev <prempravash65@gmail.com>
2026-08-17 23:04:11 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 function connectToDB() {
-
-  mongoose.connect(process.env.MONGO_URI)
+  mongoose
+    .connect(process.env.MONGO_URI)
     .then(() => {
-      console.log("server is connected to DB")
+      console.log("server is connected to DB");
     })
-    .catch(err => {
-      console.log("Error connecting to DB"," -> " , err.message)
-      process.exit(1)
-    })
-
+    .catch((err) => {
+      console.log("Error connecting to DB", " -> ", err.message);
+      process.exit(1);
+    });
 }
 
-
-module.exports = connectToDB
+module.exports = connectToDB;
